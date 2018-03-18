ab03ff3bc4c485c7018722b6d6a43036015f716c
niklasschroetler <privat@nschroetler.de>
2018-03-18 01:12:22 +0100

    Changed plugins

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-gcmpushplugin.GCMPushPlugin",
+    "file": "plugins/cordova-plugin-gcmpushplugin/www/gpp.js",
+    "pluginId": "cordova-plugin-gcmpushplugin",
+    "clobbers": [
+      "GcmPushPlugin"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-gcmpushplugin": "1.2.0"
+};
+// BOTTOM OF METADATA
+});
